28c412bf15abca37765e99966bb7feb641ab1866
Henrique Peixoto <henriquep@gmail.com>
2017-10-23 19:57:24 -0200

    Adicionado Tipo Item

---
+++ WebApplication1/WebApplication1/Models/TipoItem.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models
+{
+    public class TipoItem
+    {
+        public int ItemID { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/item.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class item
+    {
+        public int ItemID { get; set; }
+        public string Nome { get; set; }
+        public int Descricao { get; set; }
+        public int DanoMaximo { get; set; }
+        public int Raridade { get; set; }
+        //Relacionamento do Item --> TipoItem
+        public int TipoItemID { get; set; }
+        public virtual TipoItem :_TipoItem { get; set; }
+    }
+}
